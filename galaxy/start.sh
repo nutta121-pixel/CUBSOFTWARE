23a19d63b017aa66c7111d393cff682bc69128d1
ThortonEllmers <ellmersthorton@gmail.com>
2026-04-02 17:50:09 +1300

    GALAXY

--- galaxy/start.sh
+++ galaxy/start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-pip3 install -r "$(dirname "$0")/requirements.txt" --quiet
-exec python3 "$(dirname "$0")/main.py"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Create venv if it doesn't exist
+if [ ! -d "$DIR/venv" ]; then
+    python3 -m venv "$DIR/venv"
+fi
+
+# Install/update requirements
+"$DIR/venv/bin/pip" install -r "$DIR/requirements.txt" --quiet
+
+# Run the bot
+exec "$DIR/venv/bin/python3" "$DIR/main.py"

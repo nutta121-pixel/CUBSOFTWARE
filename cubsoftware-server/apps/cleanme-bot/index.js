dbfc16cac912edc2ceb5a1b9152a64bb9bfa316b
ThortonEllmers <ellmersthorton@gmail.com>
2026-04-07 22:01:45 +1200

    CUBSOFTWARE

--- cubsoftware-server/apps/cleanme-bot/index.js
+++ cubsoftware-server/apps/cleanme-bot/index.js
@@ -108,11 +108,7 @@ function saveSaves(data) {
 // Create client
 const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.DirectMessages
+        GatewayIntentBits.Guilds
     ]
 });
 
@@ -139,24 +135,6 @@ function updatePresence() {
     presenceIndex = (presenceIndex + 1) % presenceMessages.length;
 }
 
-// DM Auto-Response
-client.on('messageCreate', async (message) => {
-    if (message.author.bot) return;
-    if (message.channel.type === ChannelType.DM) {
-        const embed = new EmbedBuilder()
-            .setColor(0x5865F2)
-            .setTitle('CleanMe Bot')
-            .setDescription(
-                `Thanks for reaching out! I don't handle DMs directly.\n\n` +
-                `If you need assistance or help:\n\n` +
-                `**Join our Discord Server:**\nhttps://discord.gg/ngQXHUbnKg\n\n` +
-                `**Message the Developer:**\nhttps://discord.com/users/523949187663585310`
-            )
-            .setFooter({ text: 'CUB SOFTWARE' })
-            .setTimestamp();
-        await message.reply({ embeds: [embed] }).catch(() => {});
-    }
-});
 
 client.once('ready', async () => {
     const totalMembers = client.guilds.cache.reduce((a, g) => a + g.memberCount, 0);

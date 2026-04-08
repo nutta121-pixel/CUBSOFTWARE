4d8fa33951cecfaf396a6a145d5bf6d51a631376
ThortonEllmers <ellmersthorton@gmail.com>
2026-04-08 22:23:50 +1200

    Update main.py

--- galaxy/main.py
+++ galaxy/main.py
@@ -437,25 +437,25 @@ async def send(ctx, channel: discord.TextChannel, *, message):
         embed.set_footer(text=f'{stamp}')
         await channel.send(embed=embed)
 @client.command()
-async def vinvite(ctx, member:discord.Member, interaction:discord.Integration):
+async def vinvite(ctx, member: discord.Member):
     try:
         if ctx.author.voice:
             channel = ctx.author.voice.channel
             if "'s Private Channel" in channel.name:
                 await channel.set_permissions(member, connect=True, speak=True, view_channel=True)
-                await interaction.respnse.send_message(f"Done! {member.name} can now see your private channel!!", ephemeral=True)
+                await ctx.reply(f"Done! {member.name} can now see your private channel!!")
                 await asyncio.sleep(0.5)
                 await ctx.message.delete()
             else:
-                await interaction.respnse.send_message("You are not in a private call!", ephemeral=True)
+                await ctx.reply("You are not in a private call!")
                 await asyncio.sleep(0.5)
                 await ctx.message.delete()
         else:
-            await interaction.respnse.send_message('you must send this in a voice call', ephemeral=True)
+            await ctx.reply("you must send this in a voice call")
             await asyncio.sleep(0.5)
             await ctx.message.delete()
     except Exception as e:
-            print(f"ERROR deleting channel {e}")
+        print(f"ERROR deleting channel {e}")
 @client.command()
 async def summon(ctx):
     if ctx.author.voice:
